1dbfc2acb6b0f6e71ef0f5bed2bdfc3de38d4945
SangYong-Jeong <mmaf@naver.com>
2021-11-25 20:26:42 +0900

    서버사이드 렌더링 공부중

--- src/index.server.js
+++ src/index.server.js
@@ -5,6 +5,12 @@ import { StaticRouter } from 'react-router-dom';
 import App from './App';
 import path from 'path';
 import fs from 'fs';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import createSagaMiddleware, { END } from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+import PreloadContext from './lib/PreloadContext';
 
 // asset-manifest.json에서 파일 경로들을 조회합니다.
 const manifest = JSON.parse(
@@ -16,7 +22,7 @@ const chunks = Object.keys(manifest.files)
   .map((key) => `<script src="${manifest.files[key]}"></script>`) // 스크립트 태그로 변환하고
   .join(''); // 합침
 
-function createPage(root, tags) {
+function createPage(root, stateScript) {
   return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -35,6 +41,7 @@ function createPage(root, tags) {
       <div id="root">
         ${root}
       </div>
+      ${stateScript}
       <script src="${manifest.files['runtime-main.js']}"></script>
       ${chunks}
       <script src="${manifest.files['main.js']}"></script>
@@ -50,15 +57,44 @@ const serverRender = async (req, res, next) => {
   // 이 함수는 404가 떠야 하는 상황에 404를 띄우지 않고 서버사이드 렌더링을 해줍니다.
 
   const context = {};
-
-  const jsx = (
-    <StaticRouter location={req.url} context={context}>
-      <App />
-    </StaticRouter>
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk, sagaMiddleware)
   );
 
-  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
-  res.send(createPage(root)); // 클라이언트에게 결과물을 응답합니다.
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
+
+  const preloadContext = {
+    done: false,
+    promises: [],
+  };
+
+  const jsx = (
+    <PreloadContext.Provider value={preloadContext}>
+      <Provider store={store}>
+        <StaticRouter location={req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    </PreloadContext.Provider>
+  );
+
+  ReactDOMServer.renderToStaticMarkup(jsx); // renderToStaticMarkup으로 한번 렌더링 합니다.
+  store.dispatch(END); // redux-saga의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됩니다.
+  try {
+    await sagaPromise; // 기존에 진행 중이던 사가들이 모두 끝날 때까지 기다립니다.
+    await Promise.all(preloadContext.promises); // 모든 프라미스를 기다립니다.
+  } catch (e) {
+    return res.status(500);
+  }
+
+  const root = ReactDOMServer.renderToString(jsx); // 렌더링을 합니다.
+  // JSON 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환 처리
+  // https://redux.js.org/recipes/server-rendering#security-considerations
+  const stateString = JSON.stringify(store.getState()).replace(/<g/, '\\u003c');
+  const stateScript = `<script>__PRELOADED_STATE__=${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입합니다.
+  res.send(createPage(root, stateScript)); // 클라이언트에게 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve('./build'), {
